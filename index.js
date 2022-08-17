2572fe87aff8bb1937ee73f27915c5cea718065c
ryder <ryder.jacka@gmail.com>
2022-08-17 16:32:25 +1000

    Progress on adding book to DOM

---
+++ index.js
@@ -0,0 +1,24 @@
+let myLibrary = [{name: "The Hobbit", author: "J.R.R. Tolkien", pages: 295, read: 0}];
+const display = document.querySelector(".card-container");
+
+function Book(title, author, numPages, read) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, numPages, read) {
+  // do stuff here
+  const book = new Book(title, author, numPages, read);
+  myLibrary.push(book);
+}
+
+function displayBooks() {
+    for (i in myLibrary) {
+        const card = document.createElement("div");
+        card.classList.add('card-item');
+        display.appendChild(card);
+    }
+}
