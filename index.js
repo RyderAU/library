80254516e1ffd5349f5e48a0bc17d8d0afb151ea
ryder <ryder.jacka@gmail.com>
2022-09-18 22:58:05 +1000

    Remove and read button

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let myLibrary = [];
 const display = document.querySelector(".card-container");
 
-function Book(id, title, author, numPages, read) {
+function Book(title, author, numPages, read) {
   // the constructor...
-  this.id = id;
+  this.id = 0;
   this.title = title;
   this.author = author;
   this.numPages = numPages;
@@ -15,16 +15,13 @@ function displayForm() {
 }
 
 function addBookToLibrary(title, author, numPages, read) {
-  // do stuff here
-  const book = new Book((myLibrary.length + 1), title, author, numPages, read);
+  const book = new Book(title, author, numPages, read);
   myLibrary.push(book);
-  console.log('books id is ' + book.id)
-  displayBook(book.id, book.title, book.author, book.numPages, book.read);
+  displayBooks();
   document.querySelector(".form-container").style.display = "none";
 }
 
 function displayBook(id, title, author, numPages, read) {
-  console.log('adding one')
   const card = document.createElement("div");
   card.classList.add('card-item');
   // title
@@ -40,8 +37,7 @@ function displayBook(id, title, author, numPages, read) {
   const cardTextItemThree = document.createElement("div");
   cardTextItemThree.classList.add('card-text-item');
   cardTextItemThree.textContent = `${numPages} pages`;
-  // read status
-  console.log(read)
+  // read button
   const cardTextItemFour = document.createElement("div");
   cardTextItemFour.classList.add('card-text-item');
   if (read) {
@@ -51,12 +47,58 @@ function displayBook(id, title, author, numPages, read) {
     cardTextItemFour.classList.add('card-text-item-not-read');
     cardTextItemFour.textContent = 'not read';
   }
+
+  // change read status function
+  cardTextItemFour.onclick = function() {
+    for (let i = 0 ; i < myLibrary.length ; i++) {
+      if (i == id) {
+        myLibrary[i].read = !myLibrary[i].read;
+      }
+      displayBooks();
+    }
+  }
+
+  // remove button
+  const cardTextItemFive = document.createElement("div");
+  cardTextItemFive.classList.add('card-text-item');
+  cardTextItemFive.classList.add('card-text-item-remove');
+  cardTextItemFive.textContent = `remove`;
+  
+  // remove function
+  cardTextItemFive.onclick = function() {
+    for (let i = 0 ; i < myLibrary.length ; i++) {
+      if (i == id) {
+        myLibrary.splice(i, 1);
+      }
+      displayBooks();
+    }
+  }
+
   card.appendChild(cardTextItemOne);
   card.appendChild(cardTextItemTwo);
   card.appendChild(cardTextItemThree);
   card.appendChild(cardTextItemFour);
+  card.appendChild(cardTextItemFive);
   display.appendChild(card);
 }
+
+// go through myLibrary array and display books on DOM
+function displayBooks() {
+  // remove all current elements
+  removeChildNodes(display);
+  for (let i = 0 ; i < myLibrary.length ; i++) {
+    myLibrary[i].id = i;
+    displayBook(myLibrary[i].id, myLibrary[i].title, myLibrary[i].author, myLibrary[i].numPages, myLibrary[i].read);
+  }
+}
+
+// remove an element's child elements
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
 
 window.onclick = function(e) {
   // close modal when clicking off
